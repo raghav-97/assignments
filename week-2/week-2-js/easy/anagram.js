00d3391bfa5f47d2f360e55db2c1f79a1467f6dc
raghav-97 <raghavtandulkar97@gmail.com>
2025-02-04 20:00:42 +0530

    solved anagram.js

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
+  const cleanStr1 = str1.toLowerCase().split("").sort().join();
+  const cleanStr2 = str2.toLowerCase().split("").sort().join();
 
+  if (cleanStr1 === cleanStr2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = isAnagram;
