02deee330f6ae493d4adcdf1ed8deb0982a7b8ea
raghav-97 <raghavtandulkar97@gmail.com>
2025-02-04 20:33:18 +0530

    solved largestnumber

--- week-2/week-2-js/easy/findLargestElement.js
+++ week-2/week-2-js/easy/findLargestElement.js
@@ -6,7 +6,14 @@
 */
 
 function findLargestElement(numbers) {
-    
+  let largestEle = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] >= largestEle) {
+      largestEle = numbers[i];
+    }
+  }
+  return largestEle;
 }
 
 module.exports = findLargestElement;
+
